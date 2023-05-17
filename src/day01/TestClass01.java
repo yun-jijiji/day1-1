46295e8d8de1d3c47d91e9447c0370f9da90a933
AAA <ky10237@naver.com>
2023-05-17 18:08:44 +0900

    이클립스

--- src/day01/TestClass01.java
+++ src/day01/TestClass01.java
@@ -1,5 +1,7 @@
 package day01;
 
 public class TestClass01 {
-
+public static void main(String[] args) {
+System.out.print("뭐 어쩌라고오오오 내 인생 대신 살아줄 거냐고오오오");
+}
 }
---
+++ src/day01/TestClass02.java
@@ -0,0 +1,20 @@
+package day01;
+
+public class TestClass02 {
+public static void main(String[] args) {
+	System.out.println(
+			/*
+			 escape 문자
+			 -문자열 안에서 특수한 기능을 수행
+			 -\n :엔터값과 비슷하다
+			 -\t :커서를 탭 크기만큼 오른쪽으로 이동
+			 -\" :"하나 표현
+			 -\\ :\하나 표현
+			 */);
+			System.out.println("안\n녕\n하\n세\n요");
+			System.out.println("안녕\t하세\t요");
+			System.out.println(" ");
+			System.out.println(" '\t\t'");
+			System.out.println("\tㅅ");
+}
+}
